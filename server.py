3ec1127c72f0c38243799d454b0bd4905cf71b41
FedeLupianez <lupianezfederico@gmail.com>
2025-11-10 18:31:39 -0300

    Server local en python para poder levantar la página

---
+++ server.py
@@ -0,0 +1,17 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+
+class UTF8Handler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header("Cache-Control", "no-store")
+        if self.path.endswith((".html", ".css", ".js")):
+            self.send_header(
+                "Content-Type", self.guess_type(self.path) + "; charset=utf-8"
+            )
+        super().end_headers()
+
+
+if __name__ == "__main__":
+    server = HTTPServer(("localhost", 8000), UTF8Handler)
+    print("Servidor corriendo en http://localhost:8000")
+    server.serve_forever()
