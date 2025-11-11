37a2f72c9c2f90df79a99c131e02226b62bd173e
FedeLupianez <lupianezfederico@gmail.com>
2025-11-11 19:10:16 -0300

    Mejoro el estilo de las cards

--- Frontend/components/card.js
+++ Frontend/components/card.js
@@ -1,37 +1,27 @@
 class Card extends HTMLElement {
-  constructor() {
-    super();
-  }
+   constructor() {
+      super();
+   }
 
-  connectedCallback() {
-    this.title = this.getAttribute("title") || "";
-    this.country = this.getAttribute("country") || "";
-    this.rank = this.getAttribute("rank") || "";
-    this.price = this.getAttribute("price") || "";
-    this.image = this.getAttribute("image") || "";
+   connectedCallback() {
+      this.title = this.getAttribute("title") || "";
+      this.country = this.getAttribute("country") || "";
+      this.price = this.getAttribute("price") || "";
+      this.image = this.getAttribute("image") || "";
 
-    console.log("creating card ...")
-    this.innerHTML = `
-      <div class="card">
-         <a href="index.html" class="image-container">
-            <img src="${this.image}" />
-            <like-button style="z-index: 10;"/>
-         </a>
-         <div class="middle-card">
+      console.log("creating card ...")
+      this.innerHTML = `
+      <a href="/Frontend/routes/contact/contact.html">
+         <div class="card" style="background-image: url(${this.image}); image-rendering: Optimizespeed;">
+            <like-button></like-button>
             <div class="title-card-container">
                <span class="card-title">${this.title}</span>
                <span class="card-country">${this.country}</span>
             </div>
-            <div class="card-rank-container">
-               <span class="card-rank-number">${this.rank}</span>
-            </div>
-         </div>
-         <div class="bottom-card">
-            <span class="card-price">1 Noche</span>
             <span class="card-price">${this.price}</span>
          </div>
-      </div>
+      </a>
       `
-  }
+   }
 }
 customElements.define("card-item", Card);
