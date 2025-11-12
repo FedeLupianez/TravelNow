2a2ed8c4fd0aa43657e715f0be29bf79dacd8c78
FedeLupianez <lupianezfederico@gmail.com>
2025-11-12 18:38:51 -0300

    Agrego la UI para mostrar los contactos creados

---
+++ Frontend/routes/contact/components/contact_item/contact_item.js
@@ -0,0 +1,30 @@
+class contact_register extends HTMLElement {
+   constructor() {
+      super();
+   }
+
+   connectedCallback() {
+      this.name = this.getAttribute('name') || '';
+      this.lastname = this.getAttribute('lastname') || '';
+      this.email = this.getAttribute('email') || '';
+      this.destination = this.getAttribute('destination') || '';
+      this.number = this.getAttribute('number') || '';
+      this.subject = this.getAttribute('subject') || '';
+      this.message = this.getAttribute('message') || '';
+
+      this.innerHTML = `
+        <div class="contact-item">
+           <div class="contact-left-info">
+              <h2>${this.name} ${(this.lastname == "undefined") ? '' : this.lastname}</h2>
+              <p><strong>Email:</strong> ${this.email}</p>
+              <p><strong>Destino:</strong> ${this.destination}</p>
+              <p><strong>Teléfono:</strong> ${this.number}</p>
+              <p><strong>Asunto:</strong> ${this.subject}</p>
+           </div>
+           <p class="contact-message"><strong>Mensaje:</strong> ${this.message}</p>
+        </div>
+        `;
+   }
+
+}
+customElements.define("contact-item", contact_register);
--- Frontend/routes/contact/countries.js
+++ Frontend/routes/contact/countries.js
@@ -1,22 +1,22 @@
 const select = document.getElementById("form_destination");
 
 const set_countries = async () => {
-    const response = await fetch('https://restcountries.com/v3.1/all?fields=name,flags');
-    if (!response.ok)
-        return [];
-    
-    const data = await response.json();
+   const response = await fetch('https://restcountries.com/v3.1/all?fields=name,flags');
+   if (!response.ok)
+      return [];
 
-    for (let i = 0; i < data.length; i++) {
-        const option = document.createElement("option");
-        option.value = data[i].name.common;
-        option.innerHTML = `
+   const data = await response.json();
+
+   for (let i = 0; i < data.length; i++) {
+      const option = document.createElement("option");
+      option.value = data[i].name.common;
+      option.innerHTML = `
             <option value="${data[i].name.common}">
-                <img src="${data[i].flags.png}" alt="${data[i].name.common}"> ${data[i].name.common}
+                ${data[i].name.common}
             </option>
         `;
-        select.appendChild(option);
-    }
+      select.appendChild(option);
+   }
 
 }
 
